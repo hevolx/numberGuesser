10a1f084c2a1e6a0b96b8acd8715478c90b054d1
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2025-08-13 21:45:00 +0200

    v.1.0.0

---
+++ script.js
@@ -0,0 +1,47 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function generateTarget() {
+    return Math.ceil(Math.random() * 9);
+}
+
+function compareGuesses(currentHumanGuess, computerGuess, target) {
+    numberRange(currentHumanGuess);
+    const humanDifference = getAbsoluteDistance(currentHumanGuess, target);
+    const computerDifference = getAbsoluteDistance(computerGuess, target);
+    if ((humanDifference < computerDifference) || currentHumanGuess === target) {
+        return true; // Human is closer
+    } else if ((computerDifference < humanDifference) || computerGuess === target) {
+        return false; // Computer is closer
+    } else {
+        return true; // Tie goes to human
+    }
+}
+
+function numberRange(guess) {
+    if ((guess >= 0) && (guess <= 9)) {
+        return guess;
+    } else {
+        window.alert('Number out of range');
+    }
+}
+function getAbsoluteDistance(difference, target) {
+    difference = Math.abs(difference - target);
+    return difference;
+}
+
+function updateScore(winner) {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    } else {
+        window.alert('Error 404: Wrong input type');
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber += 1;
+}
